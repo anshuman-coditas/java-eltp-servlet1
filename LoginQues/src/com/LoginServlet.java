c78516137a0e318638249fd5e8192abb2999e280
Anshuman Kumar <anshuman.kumar@coditas.com>
2022-03-06 17:03:26 +0530

    initial commit

---
+++ LoginQues/src/com/LoginServlet.java
@@ -0,0 +1,53 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		PrintWriter out=response.getWriter();
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		if(uname.equals(pass))
+			out.println("Logged In");
+		else
+			out.println("Wrong Entries");
+		out.println("<h2>1</h2><br><h2>2</h2><input type='button' value='+'>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
